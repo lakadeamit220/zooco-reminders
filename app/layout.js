681dfc2a43e37e5edc104a6420f990e86f9101aa
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2026-07-20 16:45:42 +0530

    feat: add clear and simple lucide paw-print pwa logo

--- app/layout.js
+++ app/layout.js
@@ -30,6 +30,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/svg+xml" href="/icons/zooco-logo.svg" />
+        <link rel="apple-touch-icon" href="/icons/zooco-logo.svg" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
       </head>
